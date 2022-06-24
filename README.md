402e254916210e7bf6075840546f727f4bd23c58
loretta-dhahabu <loreydjefwa@gmail.com>
2022-06-24 12:26:40 +0300

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-My Cool Recipe readme file
+Sea Food Eatery readme file 
+# Sea Food Eatery
+
+# List of Contributors
+ Loretta Dhahabu Jefwa
+
+ # Description
+  A web application created using HTML,CSS and JavaScript with the aiam of providing information about the different types of seafoods and their ingredients.When the food name is clicked, an image of the type of food is played. It again provides the ingredients used to prepare the meal and this is because some people are allergic to some ingredients, theirfore is give the customer a chance to explore the ingredients whether they are okay with them or not. Also it allows the user to leave a comment after having a taste of the seafood.
+
+Homepage: 
+repository: https://github.com/loretta-dhahabu/Phase-1-loretta_final_project
+
+# Technologies Used 
+This website was created using HTML,CSS and JavaScript technologies. 
+
+## Support and Contact Details
+incase of any problem,any comments or any feedback, reach out to me through: email:loretta.jefwa@student.moringaschool.com alternatively you may use: loreydjefwa@gmail.com
+
+# License MIT License
+
+Copyright (c) [2022] [Loretta Dhahabu Jefwa]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+# Author 
+Loretta-Dhahabu
--- script.js
+++ script.js
@@ -1,28 +1,143 @@
 //add an eventListener when the page is loaded
+document.addEventListener("DOMContentLoaded", function () {
+  getSeaFood();
+});
+let users = [];
 
-document.addEventListener( "DOMContentLoaded", function ()
-{
-    fetchRecipes();
-} )
-let results = [];
-function fetchRecipes ()
-{
-    fetch(
-      "https://api.spoonacular.com/recipes/complexSearch?query=chicken&number=4&addRecipeInformation=true&includeIngredients=%22tomato,onion,cheese%22&type=main%20course&apiKey=f278ed7996b54d79b0c5497d4e1bc37a"
-    )
-        .then( (response) => response.json() )
-        .then( ( data ) =>
-        {
-            for ( const recipeObject of data )
-            {
-                const nameOfRecipe = document.createElement( "li" );
-                nameOfRecipe.innerText = recipeObject.results.title;
-                nameOfRecipe.addEventListener( "click", () =>
-                {
-                    getRecipe( recipeObject )
-                } );
-                document.getElementById("recipeName").appendChild(recipeList);
-              
-        }
-    })
+function getSeaFood() {
+  fetch("http://localhost:3000/meals")
+    .then((resp) => resp.json())
+    .then((data) => {
+      for (const foodObject of data) {
+        const nameList = document.createElement("li");
+        nameList.innerText = foodObject.strMeal;
+        nameList.addEventListener("click", () => {
+          renderFood(foodObject);
+        });
+        document.getElementById( "food-list" ).appendChild( nameList );
+        // customerComments(foodObject)
+      }
+    });
 }
+function renderFood(foodObject) {
+  document.getElementById("food-name").innerHTML = foodObject.strMeal;
+  document.getElementById("food-image").src = foodObject.strMealThumb;
+  document.getElementById("food-ingredients").innerHTML =
+    foodObject.Ingredients;
+  document.getElementById("comments-list").innerHTML = foodObject.Comments;
+  // customerComments(foodObject);
+}
+
+function ingredientsList(foodObject) {
+  fetch("http://localhost:3000/meals")
+    .then((resp) => resp.json())
+    .then( ( data ) =>
+    {
+      for (const foodObject of data) {
+            let foodIngr = document.getElementById("food-ingredients");
+            let ingrList = document.createElement("li");
+            ingrList.innerText = foodIngredient;
+            foodIngr.appendChild(ingrList);
+          }
+      // for (const foodObject of data) {
+      //   const itemList = document.createElement("li");
+      //   itemList.innerText = foodObject.Ingredients;
+      //   document.getElementById("food-ingredients").appendChild(itemList);
+      // }
+    });
+}
+document.getElementById("comment-form").onclick = () => {
+  let userComments = document.getElementById("comment").value;
+  let comm = document.getElementById("comments-list");
+  let commList = document.createElement("li");
+  commList.innerText = userComments;
+  comm.appendChild(commList);
+};
+
+
+
+
+
+// document.addEventListener("DOMContentLoaded", function () {
+//   fetchSeaFood();
+// });
+// function fetchSeaFood() {
+//   fetch("http://localhost:3000/meals")
+//     .then((response) => response.json())
+//     .then((data) => {
+//       for (const foodObject of data) {
+//         // console.log(data.meals.strMeal);
+
+//         let foodList = document.createElement("li");
+//         foodList.innerText = foodObject.strMeal;
+//         foodList.addEventListener("click", () => {
+//           getSeaFood(foodObject.idMeal);
+//         });
+//         document.getElementById("food-list").appendChild(foodList);
+//       }
+//     });
+// }
+
+// function getSeaFood(idMeal) {
+//   // alert(idMeal);
+//   fetch("http://localhost:3000/meals")
+//     .then((resp) => resp.json())
+//     .then((foods) => {
+//       for (foodObject of foods) {
+//         let meal = foodObject.strMeal;
+//         let mealId = foodObject.idMeal;
+//         let foodIngredients = foodObject.Ingredients;
+
+        // console.log(ingredients);
+        // let img = foodObject.strMealThumb;
+
+        // if (idMeal === mealId) {
+        //   document.getElementById("food-name").innerHTML = meal;
+        //   document.getElementById("food-image").src = img;
+          // for (let i = 0; i < foodIngredients; i++) {
+          //   console.log(foodIngredients[i]);
+          // }
+          // for (foodIngredient of foodIngredients) {
+          //   let foodIngr = document.getElementById("food-ingredients");
+          //   let ingrList = document.createElement("li");
+          //   ingrList.innerText = foodIngredient;
+          //   foodIngr.appendChild(ingrList);
+          // }
+          // document.getElementById("comment-form").onclick = () => {
+          //   let userComments = document.getElementById("comment").value;
+          //   let comm = document.getElementById("comments-list");
+          //   let commList = document.createElement("li");
+          //   commList.innerText = userComments;
+          //   comm.appendChild(commList);
+//           // };
+//         }
+//       }
+//     });
+// }
+
+
+// function customerComments(foodObject) {
+//   const commentsForm = document.getElementById("comment-form");
+//   commentsForm.addEventListener("submit", (eventFn) => {
+//     eventFn.preventDefault();
+//     const newComment = commentsForm.comment.value;
+//     const commentsArray = foodObject.Comments;
+//     commentsArray.push(newComment);
+//     addComment(foodObject);
+//   });
+// }
+
+// function addComment(food) {
+//   const fetchOptions = {
+//     method: "POST",
+//     headers: {
+//       "content-type": "Application/json",
+//       Accept: "*",
+//     },
+//     body: JSON.stringify(food),
+//   };
+
+//   fetch(`http://localhost:3000/food/${beer.idMeal}`, fetchOptions)
+//     .then((response) => response.json())
+//     .then((data) => console.log(data));
+// }
